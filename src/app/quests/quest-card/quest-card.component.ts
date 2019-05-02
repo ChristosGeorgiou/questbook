fab7d10f2e321b9ee5417c9ab74f285490b6a48b
Christos Georgiou <georgiou.christos@outlook.com>
2019-05-02 12:04:08 +0300

    ....

--- src/app/quests/quest-card/quest-card.component.ts
+++ src/app/quests/quest-card/quest-card.component.ts
@@ -1,6 +1,9 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Quest } from '../../_shared/services/models.all';
+import { Component, Input } from '@angular/core';
+import { ActionSheetController, AlertController, ModalController } from '@ionic/angular';
+import { DatabaseService } from '../../_shared/services/database.service';
+import { Quest, Referable } from '../../_shared/services/models.all';
 import { StateService } from '../../_shared/services/state.service';
+import { QuestModalComponent } from '../quest-modal/quest-modal.component';
 
 @Component({
   selector: 'app-quest-card',
@@ -8,11 +11,14 @@ import { StateService } from '../../_shared/services/state.service';
 })
 export class QuestCardComponent {
 
-  @Input() quest: Quest;
-  @Output() update = new EventEmitter<Quest>();
+  @Input() quest: Quest & Referable;
 
   constructor(
-    private state: StateService
+    private state: StateService,
+    private actionSheetCtrl: ActionSheetController,
+    private modalCtrl: ModalController,
+    private db: DatabaseService,
+    private alertCtrl: AlertController,
   ) { }
 
   get isMaster() {
@@ -28,11 +34,72 @@ export class QuestCardComponent {
       this.quest.visible = Date.now();
     }
     item.visible = Date.now();
-    this.update.emit(this.quest);
   }
 
   async hideItem(item) {
     item.visible = null;
-    this.update.emit(this.quest);
+  }
+
+  async showMenu() {
+    if (!this.state.campaign$.value.isMaster) { return; }
+    const actionSheet = await this.actionSheetCtrl.create({
+      buttons: [
+        { text: 'Show', icon: 'eye', handler: () => { this.show(); } },
+        { text: 'Hide', icon: 'eye-off', handler: () => { this.hide(); } },
+        { text: 'Remove', icon: 'trash', handler: () => { this.remove(); } },
+        { text: 'Edit', icon: 'share', handler: () => { this.edit(); } },
+        { text: 'Cancel', icon: 'close', role: 'cancel' }
+      ]
+    });
+    await actionSheet.present();
+  }
+
+  async show() {
+    this.quest.visible = Date.now();
+    await this.db.update(this.quest.ref, this.quest);
+  }
+
+  async hide() {
+    this.quest.visible = null;
+    await this.db.update(this.quest.ref, this.quest);
+  }
+
+  async edit() {
+    const modal = await this.modalCtrl.create({
+      component: QuestModalComponent,
+      componentProps: {
+        quest: { ...this.quest }
+      }
+    });
+
+    modal.onDidDismiss().then(async (res) => {
+      const newq: Quest & Referable = res.data;
+      await this.db.update(this.quest.ref, newq);
+    });
+
+    await modal.present();
+  }
+
+  async remove() {
+    const alert = await this.alertCtrl.create({
+      header: 'Confirm Remove',
+      subHeader: 'This quest will be removed from all players. This action is not reversible.',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Remove',
+          handler: async () => {
+            await this.db.remove(this.quest.ref);
+          }
+        }
+      ]
+    });
+    await alert.present();
   }
 }
--- src/app/quests/quests.component.ts
+++ src/app/quests/quests.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
-import { ActionSheetController, AlertController, ModalController } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { DatabaseService } from '../_shared/services/database.service';
@@ -17,10 +17,8 @@ export class QuestsComponent implements OnInit {
 
   constructor(
     private state: StateService,
-    private actionSheetCtrl: ActionSheetController,
     private modalCtrl: ModalController,
     private db: DatabaseService,
-    private alertCtrl: AlertController,
   ) { }
 
   async ngOnInit() {
@@ -34,91 +32,29 @@ export class QuestsComponent implements OnInit {
       );
   }
 
-  async questForm(quest: Quest) {
+  getRef(i, q: Referable) {
+    return q.ref;
+  }
 
-    const q = quest || {
-      visible: null,
-      items: [{
-        visible: null
-      }]
-    };
-
+  async create() {
     const modal = await this.modalCtrl.create({
       component: QuestModalComponent,
       componentProps: {
-        quest: { ...q }
+        quest: {
+          visible: null,
+          items: [{
+            visible: null
+          }]
+        }
       }
     });
 
     modal.onDidDismiss().then(async (res) => {
       const newq: Quest & Referable = res.data;
-      if (newq.ref) {
-        this.updateQuest(newq);
-      } else {
-        await this.db.add<Quest>(DocType.quest, newq);
-      }
+      await this.db.add<Quest>(DocType.quest, newq);
     });
 
     await modal.present();
-  }
-
-  async presentActionSheet(quest: Quest) {
-    if (!this.state.campaign$.value.isMaster) { return }
-    const actionSheet = await this.actionSheetCtrl.create({
-      buttons: [{
-        text: 'Show', icon: 'eye', handler: () => { this.showQuest(quest); }
-      }, {
-        text: 'Hide', icon: 'eye-off', handler: () => { this.hideQuest(quest); }
-      }, {
-        text: 'Remove', icon: 'trash', handler: () => { this.removeQuest(quest); }
-      }, {
-        text: 'Edit', icon: 'share', handler: () => { this.questForm(quest); }
-      }, {
-        text: 'Cancel', icon: 'close', role: 'cancel', handler: () => { }
-      }]
-    });
-    await actionSheet.present();
-  }
-
-  async showQuest(quest) {
-    quest.visible = Date.now();
-    await this.updateQuest(quest);
-  }
-
-  async hideQuest(quest) {
-    quest.visible = null;
-    await this.updateQuest(quest);
-  }
-
-  async updateQuest(quest: Quest & Referable) {
-    await this.db.update(quest.ref, quest);
-  }
-
-  async removeQuest(quest) {
-    const alert = await this.alertCtrl.create({
-      header: 'Confirm Remove',
-      subHeader: 'This quest will be removed from all players. This action is not reversible.',
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          handler: () => {
-            console.log('Cancel clicked');
-          }
-        },
-        {
-          text: 'Remove',
-          handler: async () => {
-            await this.db.remove(quest.ref);
-          }
-        }
-      ]
-    });
-    await alert.present();
-  }
-
-  getRef(i, q: Referable) {
-    return q.ref;
   }
 }
 
